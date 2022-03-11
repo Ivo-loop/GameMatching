f79cfbbcc7178bd4199169793bb64625c3dcf226
Natanael Junkes <natanael.junkes@ambevtech.com.br>
2022-03-11 16:15:26 -0300

    refac: alteração do nome para Jogo para não confundir com o namespace.

---
+++ GameMatching/Jogos/Entidades/Jogo.cs
@@ -0,0 +1,10 @@
+﻿using GameMatching.Comum.Entidades;
+
+namespace GameMatching.Jogos.Entidades
+{
+    public class Jogo : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public int QuantidadeJogadores { get; set; }
+    }
+}
