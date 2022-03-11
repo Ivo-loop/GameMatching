a4c5ea93c5baee99adc88ab8deae7617ae91cb37
ivo.junior <ivo.junior@ambevtech.com.br>
2022-03-11 16:08:49 -0300

    add entidade SolicitacaoPlayer

---
+++ GameMatching/SolicitacaoPlayer/Entidades/SolicitacaoPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GameMatching.SolicitacaoPlayer.Entidades
+{
+    public class SolicitacaoPlayer
+    {
+        public Guid IdPartida { get; set; }
+        public Guid IdJogo { get; set; }
+    }
+}
