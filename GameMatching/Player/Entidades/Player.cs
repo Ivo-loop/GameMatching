20815e4fddb6fe5f5c35e8431d24d3d5ca5796fb
Filipe Eduardo Regis <filipeeduardoregis2017@gmail.com>
2022-03-11 16:08:46 -0300

    Entidade player adicionada

---
+++ GameMatching/Player/Entidades/Player.cs
@@ -0,0 +1,10 @@
+using GameMatching.Comum.Entidades;
+using System;
+
+namespace GameMatching.Player.Entidades
+{
+    public class Player : EntidadeBase
+    {
+        public string Nome { get; set; }
+    }
+}
