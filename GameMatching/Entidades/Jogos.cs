0a78e8687dbf76f04b9b60ffa242336d5fe3e119
Natanael Junkes <natanael.junkes@ambevtech.com.br>
2022-03-11 16:01:46 -0300

    feature: adiciona a pasta Entidades e uma classe entidade Jogos

---
+++ GameMatching/Entidades/Jogos.cs
@@ -0,0 +1,10 @@
+﻿using GameMatching.Comum.Entidades;
+
+namespace GameMatching.Entidades
+{
+    public class Jogos : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public int QuantidadeJogadores { get; set; }
+    }
+}
