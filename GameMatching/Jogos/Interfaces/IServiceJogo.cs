36f623c7b32650ad7c8f96190b4959865807be93
Natanael Junkes <natanael.junkes@ambevtech.com.br>
2022-03-11 16:32:17 -0300

    feature: adicionado um Service para o Jogo e uma interface para o service.

---
+++ GameMatching/Jogos/Interfaces/IServiceJogo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GameMatching.Jogos.Entidades;
+
+namespace GameMatching.Jogos.Interfaces
+{
+    public interface IServiceJogo
+    {
+        void Cadastrar(string nomeJogo, int quantidadeJogadores);
+        List<Jogo> BuscarTodos();
+    }
+}
